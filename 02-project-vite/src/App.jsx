573f41866a30261e55a1b87b503450ecc4fcb7db
hviveros <humberto.viverosrios@gmail.com>
2025-07-07 21:06:22 -0300

    Capítulo 2 - Primera app

---
+++ 02-project-vite/src/App.jsx
@@ -0,0 +1,24 @@
+// import './App.css'
+
+// function App() {
+//   return (
+//     <>
+//       <div className="App">
+//         <h1>Vite & React</h1>
+//       </div>
+//     </>
+//   )
+// }
+
+// Como arrow function
+const App = () => {
+  return (
+    <>
+      <div className="App">
+        <h1>Vite & React</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
